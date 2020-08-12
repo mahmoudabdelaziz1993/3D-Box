e26cf33cbeb1aa65f9cc2a86eb98666346b6b31c
mahmoudabdelaziz1993 <mahmoudabdelaziz1993@outlook.com>
2020-08-12 23:43:59 +0200

    canvas with 3d box and text-header

--- src/App.js
+++ src/App.js
@@ -1,13 +1,30 @@
 import React from 'react';
 import './App.scss';
 
+// setup react three fiber  canvas
+import { Canvas } from 'react-three-fiber'
+import Box from './components/Box';
+
+
+
 function App() {
   return (
-    <div className="App">
-     <h1>
-       Hello
-     </h1>
+    <div className='wrapper'>
+    <Canvas id='canv'>
+
+      <ambientLight />
+      <pointLight position={[10, 10, 10]} />
+
+      <Box />
+    </Canvas>
+    <div className='heading'>
+      <h1> 3D scene for a Box Geometry </h1> 
+     
     </div>
+    </div>
+
+
+
   );
 }
 
