83478992a940692b6034e282ddadc339afd38c3d
mahmoudabdelaziz1993 <mahmoudabdelaziz1993@outlook.com>
2020-08-15 00:53:18 +0200

    orbit controls

--- src/App.js
+++ src/App.js
@@ -4,23 +4,25 @@ import './App.scss';
 // setup react three fiber  canvas
 import { Canvas } from 'react-three-fiber'
 import Box from './components/Box';
+import { OrbitControls } from 'drei'
 
 
 
 function App() {
   return (
     <div className='wrapper'>
-    <Canvas id='canv'>
+      <Canvas id='canv'>
 
-      <ambientLight />
-      <pointLight position={[10, 10, 10]} />
+        <OrbitControls />
+        <ambientLight />
+        <pointLight position={[10, 10, 10]} />
 
-      <Box />
-    </Canvas>
-    <div className='heading'>
-      <h1> 3D scene for a Box Geometry </h1> 
-     
-    </div>
+        <Box />
+      </Canvas>
+      <div className='heading'>
+        <h1> 3D scene for a Box Geometry </h1>
+
+      </div>
     </div>
 
 
